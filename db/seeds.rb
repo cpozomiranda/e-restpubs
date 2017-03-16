1f30ce204f4f7ab3c483620c34e186837242a803
Cesar Pozo <c.pozomiranda@gmail.com>
2017-03-16 19:56:27 -0300

    models and seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name:'Cocktails')
+Category.create(name:'Energy Drinks')
+Category.create(name:'Caribbean Drinks')
+Category.create(name:'Cervezas')
+Category.create(name:'Shots')
+Category.create(name:'Scotch')
+Category.create(name:'Pisco')
+Category.create(name:'Ron')
+
+Product.create(name:'Manhattan', price:'3500', photo: , category_id:Category.find_by(name:'Cocktails').id)
+Product.create(name:'Cosmopolitan', price:'3500', photo: , category_id:Category.find_by(name:'Cocktails').id)
+Product.create(name:'Laguna Azul', price:'3500', photo: , category_id:Category.find_by(name:'Cocktails').id)
+Product.create(name:'Daiquiri', price:'3500', photo: , category_id:Category.find_by(name:'Cocktails').id)
+
+Product.create(name:'Redbull' price:'3500', photo: , category_id:Category.find_by(name:'Energy Drinks').id)
+Product.create(name:'Speed', price:'3500', photo: , category_id:Category.find_by(name:'Energy Drinks').id)
+Product.create(name:'Battery', price:'3500', photo: , category_id:Category.find_by(name:'Energy Drinks').id)
+Product.create(name:'Monster', price:'3500', photo: , category_id:Category.find_by(name:'Energy Drinks').id)
